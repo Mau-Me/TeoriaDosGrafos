b56dd72e591c05435fcda46ff4483a34b84f465c
Mau-Me <mauriciosm95@gmail.com>
2020-12-03 04:14:28 -0300

    Adicionado novo tipo de dados(vertice)

--- Trabalho01/Implementacao/AlgoritmoInundacao/matrizAdjacencias.h
+++ Trabalho01/Implementacao/AlgoritmoInundacao/matrizAdjacencias.h
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #ifndef MATRIZADJACENCIAS_H_INCLUDED
 #define MATRIZADJACENCIAS_H_INCLUDED
 
-void lerMatriz(int matriz[6][6]);
-void inundacao(int matriz[6][6], int vertice);
-void tipoGrafo(int matriz[6][6]); // Indica se grafo é Conexo ou Desconexo
+typedef struct vertice {
+    int num;
+    bool flag;
+} Vertice;
+
+void lerMatriz(Vertice matriz[6][6]);
+void inundacao(Vertice matriz[6][6], int vertice);
+void tipoGrafo(Vertice matriz[6][6]); // Indica se grafo é Conexo ou Desconexo
 
 
 #endif // MATRIZADJACENCIAS_H_INCLUDED
