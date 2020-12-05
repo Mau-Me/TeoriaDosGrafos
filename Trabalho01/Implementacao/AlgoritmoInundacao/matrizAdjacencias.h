ef1c708e8cfd2c36c381637e760d584ddeebfbb7
Mau-Me <mauriciosm95@gmail.com>
2020-12-05 06:55:32 -0300

    alteração da estrutura Vertice, e add declaração de novas funcs.

--- Trabalho01/Implementacao/AlgoritmoInundacao/matrizAdjacencias.h
+++ Trabalho01/Implementacao/AlgoritmoInundacao/matrizAdjacencias.h
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 #ifndef MATRIZADJACENCIAS_H_INCLUDED
 #define MATRIZADJACENCIAS_H_INCLUDED
 
+#define TamMatriz 2
+
 typedef struct vertice {
-    int num;
-    bool flag;
+    char nomeVertice;
+    bool flagInundado;
+    bool *adjacencias;
 } Vertice;
 
-void lerMatriz(Vertice matriz[6][6]);
-void inundacao(Vertice matriz[6][6], int vertice);
-void tipoGrafo(Vertice matriz[6][6]); // Indica se grafo é Conexo ou Desconexo
+void lerMatriz(Vertice vertices[TamMatriz]);
+void imprimeMatriz(Vertice vertices[TamMatriz]);
+void inundacao(Vertice matriz[TamMatriz], int vertice);
+void tipoGrafo(Vertice matriz[TamMatriz]); // Indica se grafo é Conexo ou Desconexo
+
+
 
 
 #endif // MATRIZADJACENCIAS_H_INCLUDED
