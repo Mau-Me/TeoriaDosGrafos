ac3f155b4b9312979cf9b0e4b8824cde6d5f0b90
Mau-Me <mauriciosm95@gmail.com>
2020-12-05 06:56:38 -0300

    implementação da func lerMatriz.

--- Trabalho01/Implementacao/AlgoritmoInundacao/matrizAdjacencias.c
+++ Trabalho01/Implementacao/AlgoritmoInundacao/matrizAdjacencias.c
@@ -1,17 +1,47 @@
 #include "matrizAdjacencias.h"
 
 
-void lerMatriz(int matriz[6][6])
-{
+void lerMatriz(Vertice vertices[TamMatriz]){
+    char tempAdjacencia;
 
+    for(int i=0; i<TamMatriz; i++ ) {
+        printf("Qual o nome do %d vertice?(1 Caracter)\n", i+1);
+        scanf(" %c", &vertices[i].nomeVertice);
+        vertices[i].flagInundado=false;
+        vertices[i].adjacencias=malloc(TamMatriz * sizeof(bool));
+    }
+
+    printf("\n\nPreencha as adjacencias dos vertices\n\n");
+
+    for(int i=0; i<TamMatriz; i++){
+        for (int j = 0; j < TamMatriz; j++){
+            do{
+                printf("%c e %c sao adjacentes? (1 para sim, 0 para nao)\n", vertices[i].nomeVertice, vertices[j].nomeVertice);
+                scanf(" %c", &tempAdjacencia);
+            }
+            while(tempAdjacencia != '0' && tempAdjacencia != '1');
+
+            if(tempAdjacencia == '1'){
+                vertices[i].adjacencias[j]=true;
+            }
+            else{
+                vertices[i].adjacencias[j]=false;
+            }
+        }
+    }
 }
 
-void inundacao(int matriz[6][6], int vertice)
-{
-
+void imprimeMatriz(Vertice vertices[TamMatriz]){
+    for(int i=0; i<TamMatriz; i++ ) {
+        for(int j=0; j<TamMatriz; j++) {
+            printf("%d\t", vertices[i].adjacencias[j]);
+        }
+        printf("\n");
+    }
 }
 
-void tipoGrafo(int matriz[6][6])
-{
+void inundacao(Vertice matriz[TamMatriz], int vertice){
+}
 
+void tipoGrafo(Vertice matriz[TamMatriz]){
 }
